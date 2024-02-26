37389cc68f15b1d79c3a969c047ca77489e2ac8d
Greg Wanner <greg@tegogroup.com>
2024-02-26 15:58:30 -0500

    Created new Car class and declared variable using new Car class

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Classes
+﻿using Cars;
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            Cars.Cars Car1 = new Cars.Cars();
+            Car1.Make = "Honda";
+            Car1.Model = "Accord";
+            Car1.Year = 2020;
+
+            Console.WriteLine(Car1.Make);
+            Console.WriteLine(Car1.Model);
+            Console.WriteLine(Car1.Year);
+
         }
     }
 }
